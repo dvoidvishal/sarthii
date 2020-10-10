9e24bd8b365100014c7f9c5ff3d327b099b37a90
vishal kumar <dvoidvishal@gmail.com>
2020-10-10 21:20:11 +0530

    login and signup activity updated

--- app/src/main/java/com/example/sarthiithetuitionfinder/Activity/SignupActivity.java
+++ app/src/main/java/com/example/sarthiithetuitionfinder/Activity/SignupActivity.java
@@ -2,12 +2,22 @@ package com.example.sarthiithetuitionfinder.Activity;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RadioButton;
 import android.widget.Toast;
 
 import com.example.sarthiithetuitionfinder.DashBoard;
@@ -20,17 +30,27 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class SignupActivity extends AppCompatActivity implements View.OnClickListener {
 
-    EditText name, email, password;
+    EditText name, email, password, adress;
     Button sigup, login;
+    ImageView btn;
+    RadioButton r1,r2;
+    LocationManager lm;
+    String address;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        lm= (LocationManager) getSystemService(LOCATION_SERVICE);
+
 
         name = new EditText(this);
         email = new EditText(this);
@@ -42,11 +62,61 @@ public class SignupActivity extends AppCompatActivity implements View.OnClickLis
 
         sigup = new Button(this);
         login = new Button(this);
+        btn= (ImageView) findViewById(R.id.imageView);
+        adress= (EditText) findViewById(R.id.addressbox);
+        r1= (RadioButton) findViewById(R.id.radioButton1);
+        r2= (RadioButton) findViewById(R.id.radioButton2);
 
         sigup = findViewById(R.id.authSignupBtn);
         login = findViewById(R.id.authSignupLoginBtn);
         sigup.setOnClickListener(this);
         login.setOnClickListener(this);
+
+        //code to get location of user
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(ActivityCompat.checkSelfPermission(SignupActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION)!= PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(SignupActivity.this,Manifest.permission.ACCESS_FINE_LOCATION)!=PackageManager.PERMISSION_GRANTED){
+                    ActivityCompat.requestPermissions(SignupActivity.this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION,Manifest.permission.ACCESS_FINE_LOCATION},0);
+                    return;
+                }
+
+
+                lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, new LocationListener() {
+                    @Override
+                    public void onLocationChanged(Location location) {
+                        double lat=location.getLatitude();
+                        double lol=location.getLongitude();
+
+                        Geocoder data=new Geocoder(SignupActivity.this);
+
+                        try{
+                            List<Address> list=data.getFromLocation(lat,lol,1);
+                            address=list.get(0).getAddressLine(0);
+                        }
+                        catch (Exception e){}
+                    }
+
+                    @Override
+                    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+                    }
+
+                    @Override
+                    public void onProviderEnabled(String provider) {
+
+                    }
+
+                    @Override
+                    public void onProviderDisabled(String provider) {
+                        Toast.makeText(SignupActivity.this, "Turn on Location!", Toast.LENGTH_LONG).show();
+                    }
+                });
+                adress.setText(address);
+
+            }
+        });
 
 
     }
